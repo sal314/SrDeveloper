00ac9f80ecf1f73094630ce6c1341127d6867983
sal314 <sal352@mail2musician.com>
2017-04-13 19:17:45 -0400

    Create Database.sql

---
+++ f39dcfccffe8644a1c329b865de82952/CodeIgniter/Database.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS size_geometry;
+CREATE TABLE size_geometry (
+   hg_size INT(10),
+   hg_spaces INT(10));
+
+DROP TABLE IF EXISTS lines_geometry;
+CREATE TABLE lines_geometry (
+   l_number INT(10),
+   l_size INT(10));
+
+DROP TABLE IF EXISTS results;
+CREATE TABLE results (
+   hg_size INT(10),
+   hg_percent INT(10),
+   string VARCHAR(500));
+
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (2, 4);
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (3, 9);
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (4, 16);
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (5, 25);
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (6, 36);
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (7, 49);
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (8, 64);
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (9, 81);
+INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (10, 100);
+
+INSERT INTO lines_geometry (l_number, l_size) VALUES (1, 1);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (2, 3);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (3, 5);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (4, 7);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (5, 9);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (6, 11);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (7, 13);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (8, 15);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (9, 17);
+INSERT INTO lines_geometry (l_number, l_size) VALUES (10, 19);
+
+
