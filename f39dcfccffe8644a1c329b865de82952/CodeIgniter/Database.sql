3616442b801b25a12e86e04802e67590c005e7e0
sal314 <sal352@mail2musician.com>
2017-04-14 16:25:32 -0400

    Add files via upload

--- f39dcfccffe8644a1c329b865de82952/CodeIgniter/Database.sql
+++ f39dcfccffe8644a1c329b865de82952/CodeIgniter/Database.sql
@@ -1,18 +1,21 @@
 DROP TABLE IF EXISTS size_geometry;
 CREATE TABLE size_geometry (
    hg_size INT(10),
-   hg_spaces INT(10));
+   hg_spaces INT(10),
+   PRIMARY KEY (hg_size));
 
 DROP TABLE IF EXISTS lines_geometry;
 CREATE TABLE lines_geometry (
    l_number INT(10),
-   l_size INT(10));
+   l_size INT(10),
+   PRIMARY KEY (l_number));
 
 DROP TABLE IF EXISTS results;
 CREATE TABLE results (
    hg_size INT(10),
    hg_percent INT(10),
-   string VARCHAR(500));
+   string VARCHAR(500),
+   PRIMARY KEY (hg_size, hg_percent));
 
 INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (2, 4);
 INSERT INTO size_geometry (hg_size, hg_spaces) VALUES (3, 9);
@@ -34,5 +37,9 @@ INSERT INTO lines_geometry (l_number, l_size) VALUES (7, 13);
 INSERT INTO lines_geometry (l_number, l_size) VALUES (8, 15);
 INSERT INTO lines_geometry (l_number, l_size) VALUES (9, 17);
 INSERT INTO lines_geometry (l_number, l_size) VALUES (10, 19);
+
+
+
+
 
 
